3d438c1d1775f87c1cf2e62d6fa801b18337b540
Tripxp1 <144802453+Tripxp1@users.noreply.github.com>
2026-04-22 11:37:08 +0300

    Update TimedDoor.cpp

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,2 +1,41 @@
 // Copyright 2021 GHA Test Team
 #include "TimedDoor.h"
+#include <stdexcept>
+
+DoorTimerAdapter::DoorTimerAdapter(TimedDoor& d) : door(d) {}
+
+void DoorTimerAdapter::Timeout() {
+    if (door.isDoorOpened()) {
+        door.throwState();
+    }
+}
+
+TimedDoor::TimedDoor(int timeout) : iTimeout(timeout), isOpened(false) {
+    adapter = new DoorTimerAdapter(*this);
+}
+
+bool TimedDoor::isDoorOpened() {
+    return isOpened;
+}
+
+void TimedDoor::unlock() {
+    isOpened = true;
+}
+
+void TimedDoor::lock() {
+    isOpened = false;
+}
+
+int TimedDoor::getTimeOut() const {
+    return iTimeout;
+}
+
+void TimedDoor::throwState() {
+    throw std::runtime_error("Door is open");
+}
+
+void Timer::tregister(int, TimerClient*) {
+}
+
+void Timer::sleep(int) {
+}

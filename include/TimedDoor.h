76b8a908cfaa2e80847ac5e66baafc2b45d29816
Tripxp1 <144802453+Tripxp1@users.noreply.github.com>
2026-06-03 12:10:18 +0300

    Update TimedDoor.h

--- include/TimedDoor.h
+++ include/TimedDoor.h
@@ -35,6 +35,7 @@ class TimedDoor : public Door {
   bool isOpened;
  public:
   explicit TimedDoor(int);
+  ~TimedDoor();  // добавлен деструктор
   bool isDoorOpened();
   void unlock();
   void lock();
